2ed6084074976699e85a88e790413806aa778730
hoangtc125 <anhhoangday.69@gmail.com>
2022-04-10 13:37:18 +0700

    update toast homework landscape mode

--- ToastHomework/app/src/main/java/com/example/toasthomework/ToastHomework.java
+++ ToastHomework/app/src/main/java/com/example/toasthomework/ToastHomework.java
@@ -3,6 +3,7 @@ package com.example.toasthomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,10 +13,12 @@ public class ToastHomework extends AppCompatActivity {
 
     private TextView mShowCount;
     private Button btnCount, btnZero;
+    private String TAG = "TAG";
     private int colorOdd = -999999999,
                 colorEven = -99999,
                 colorZero = 999,
                 colorNonZero = -99999;
+    private String num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,10 @@ public class ToastHomework extends AppCompatActivity {
         btnZero = this.findViewById(R.id.button_set_zero);
         btnCount.setBackgroundColor(colorEven);
         btnZero.setBackgroundColor(colorZero);
+        Log.i(TAG, "onCreate: " + Variables.counter);
+        if(Variables.counter != null) {
+            mShowCount.setText(Variables.counter);
+        }
     }
 
     public void showToast(View view) {
@@ -34,7 +41,8 @@ public class ToastHomework extends AppCompatActivity {
 
     public void countUp(View view) {
         btnZero.setBackgroundColor(colorNonZero);
-        mShowCount.setText((Integer.parseInt(mShowCount.getText().toString()) + 1) + "");
+        Variables.setCounter((Integer.parseInt(mShowCount.getText().toString()) + 1) + "");
+        mShowCount.setText(Variables.counter);
         int value = Integer.parseInt(mShowCount.getText().toString());
         if(value > 10) {
             mShowCount.setTextSize(60);
@@ -47,6 +55,7 @@ public class ToastHomework extends AppCompatActivity {
     public void handleZero(View view) {
         btnCount.setBackgroundColor(colorEven);
         btnZero.setBackgroundColor(colorZero);
-        mShowCount.setText("0");
+        Variables.setCounter("0");
+        mShowCount.setText(Variables.counter);
     }
 }
---
+++ ToastHomework/app/src/main/java/com/example/toasthomework/Variables.java
@@ -0,0 +1,9 @@
+package com.example.toasthomework;
+
+public class Variables {
+    public static String counter;
+
+    public static void setCounter(String c) {
+        counter = c;
+    }
+}
